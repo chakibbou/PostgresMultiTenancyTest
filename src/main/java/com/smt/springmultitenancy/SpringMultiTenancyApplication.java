dd080e8eb619c38398014ad1e908dc5a9807148b
chakibbou <chakib.boukhaddaja@adad.io>
2023-11-11 14:24:50 +0100

    add configuration for local postgres container

--- src/main/java/com/smt/springmultitenancy/SpringMultiTenancyApplication.java
+++ src/main/java/com/smt/springmultitenancy/SpringMultiTenancyApplication.java
@@ -2,8 +2,11 @@ package com.smt.springmultitenancy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoReactiveAutoConfiguration.class)
+@EnableR2dbcRepositories
 public class SpringMultiTenancyApplication {
 
 	public static void main(String[] args) {
